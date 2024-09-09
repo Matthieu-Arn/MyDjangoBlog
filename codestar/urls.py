1a13dd672badec1f25b95ec2d3199452eb2de78c
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-09-09 12:33:41 +0000

    Initial commit

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,5 +20,5 @@ from blog.views import my_blog
 
 urlpatterns = [
     path("blog/", blog_views.my_blog, name='blog'),
-    path('admin/', admin.site.urls),/
+    path('admin/', admin.site.urls),
 ]
